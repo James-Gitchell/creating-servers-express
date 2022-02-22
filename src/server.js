332bdc398a434e031862bea0f3b82709bb0eeb94
James Gitchell <jamesgitchell.dev@gmail.com>
2022-02-22 13:34:04 -0500

    fixed folder path

--- src/server.js
+++ src/server.js
@@ -1,1 +1,5 @@
-const { PORT = 5000}
+const { PORT = 5000 } = process.env;
+const app = require("./app");
+
+const listener = () => console.log(`Listening on Port ${PORT}!`);
+app.listen(PORT, listener);
